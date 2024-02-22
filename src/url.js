d2d6a4a41f1fd3e280de517956bbc7ab4321e3ec
Rohit Kumar Sahu <91247476+rohit0796@users.noreply.github.com>
2024-02-22 14:35:51 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
- const url = 'http://localhost:5000/api' //https://chatapp-y8yi.onrender.com/api
+ const url = 'https://chatapp-y8yi.onrender.com/api' //http://localhost:5000/api
  export default url;
